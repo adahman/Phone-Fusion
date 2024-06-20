25fe23c9dd2232b69821cfc8476895ca30a25bd2
Madison Moulder <madmogalaxy89@att.net>
2024-06-20 12:51:05 -0400

    about us done

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,10 +2,25 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div id="about">
+      <h1 >About Us</h1>
+      <h3 id="brand">Phone-Fusion</h3>
+      <p> At Phone-Fusion, we are dedicated to providing the most 
+        high-end, quality mobile devices that are available on 
+        the market! With fast shipping times and a variety of 
+        warranties to choose from, we are there for you when you 
+        need us the most! </p>
+        <h3>Who We Are</h3>
+        <p>Thomas "Python Princess" Graham III</p>
+        <p>Abdul "Database Duke" Dahman </p>
+        <p>Madison "Style Sultan" Moulder</p>
     </div>
+
+    
   );
 };
 
-export default AboutPage;
+
+
+export default AboutPage; 
+
